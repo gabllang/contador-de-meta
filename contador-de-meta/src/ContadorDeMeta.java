354701ae431e8a6944fd827b26820fffa304ef28
Gabriel <davgabie@gmail.com>
2023-09-03 10:03:58 -0300

    primeira atualização feita

---
+++ contador-de-meta/src/ContadorDeMeta.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ContadorDeMeta {
+    public static void main(String[] args) throws Exception {
+
+        // aqui declaro as variaveis, vou dividir a pontuação esperada pelo total de series, que vai dar a media dos pontos por serie
+
+        int totalSerie = 20; 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("");
+                System.out.println("Quanto pretende fazer?");
+                    int meta = scanner.nextInt();
+
+            if (meta > 601 ) {
+                System.out.println("impossivel fazer essa pontuação");
+            }   else {
+
+                
+                    int media = meta / totalSerie;
+                    int totalPonto = totalSerie / 3;
+
+        System.out.println("");
+
+        System.out.println("se você deseja fazer " + meta + " você vai precisar fazer em media " + media + " pontos por serie, fazendo no minimo " + totalPonto +  " pontos por flecha." );
+
+        }
+
+        System.out.println("boa sorte!");
+        System.out.println("");
+
+        
+    }
+}
